6e5d3d7c3973cc98d5efd7d2d51a3e310344ed14
zulhan <zulhant270792@gmail.com>
2022-02-08 19:34:52 +0700

    update validation

--- config.go
+++ config.go
@@ -5,4 +5,7 @@ import "regexp"
 var (
 	EMAIL_FORMAT = regexp.MustCompile("^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$")
 	PHONE_FORMAT = regexp.MustCompile(`^(\+62|62|0)8[1-9][0-9]{6,10}$`)
+	URL_FORMAT   = regexp.MustCompile(`((http|https)://)(www.)?” 
++ “[a-zA-Z0-9@:%._\\+~#?&//=]{2,256}\\.[a-z]” 
++ “{2,6}\\b([-a-zA-Z0-9@:%._\\+~#?&//=]*)`)
 )
---
+++ validation.go
@@ -0,0 +1,26 @@
+package regexvalidation
+
+func ValidateEmail(email string) bool {
+
+	if !EMAIL_FORMAT.MatchString(email) {
+		return false
+	}
+
+	return true
+}
+
+func ValidatePhone(phone string) bool {
+
+	if !PHONE_FORMAT.MatchString(phone) {
+		return false
+	}
+
+	return true
+}
+
+func ValidUrl(url string) bool {
+	if !URL_FORMAT.MatchString(url) {
+		return false
+	}
+	return true
+}
